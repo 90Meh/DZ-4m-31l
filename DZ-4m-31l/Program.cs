1d0ce0ffe141171f04bc678aaf2e10564fcf584e
admin <admin@DESKTOP-TOG76HK>
2022-09-18 22:37:40 +0800

    Add project files.

---
+++ DZ-4m-31l/Program.cs
@@ -0,0 +1,24 @@
+﻿
+OtusDictionary otusDictionary = new OtusDictionary();
+
+
+class OtusDictionary
+{
+    public int counter = 0;
+    //Массивы для слова
+    private int[] arrKeys = new int[32];
+    private string[] arrValues = new string[32];
+
+    //Методы словаря 
+    public void Add(int key, string value)
+    {
+        arrKeys[counter] = key;
+        arrValues[counter] = value;
+        counter++;
+    }
+    public string Get(int key)
+    {
+        return "";
+    }
+}
+
