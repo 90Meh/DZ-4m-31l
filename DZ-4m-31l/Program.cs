5686c4ee3718b0e3d6d29130e61a739485a772a0
Meh90 <mehonoshin90@gmail.com>
2022-09-19 17:08:32 +0800

    1

--- DZ-4m-31l/Program.cs
+++ DZ-4m-31l/Program.cs
@@ -1,25 +1,73 @@
 ﻿
+//Создание объекта класса словарь.
 OtusDictionary otusDictionary = new OtusDictionary();
 
-
-class OtusDictionary
+//Цикл для заполния словаря.
+for (int key = 0; key <= 50; key++)
 {
+    otusDictionary.Add(key, RandomVord());
+}
+
+otusDictionary.Add(110, "Jopa");
+//Цикл вывода в консоль.
+for (int i = 0; i < otusDictionary.counter; i++)
+{
+    Console.WriteLine($"Элемент {otusDictionary.arrKeys[i]}");
+    Console.WriteLine($"Значение {otusDictionary.Get(i)}");
+}
+
+Console.WriteLine(otusDictionary.Get(110));
+Console.ReadKey();
+
+//Вспомогательный метод для создания случайных слов.
+string RandomVord()
+{
+    Random r = new Random();
+    string s = "";
+    for (int i = 0; i < 5; i++)
+    {
+        char a = (char)r.Next(0, 255);
+        s += a;
+    }
+    return s;
+}
+
+
+//Класс Словарь
+internal class OtusDictionary
+{
+    //Счётчик размера
     public int counter = 0;
     //Массивы для слова
-    private int[] arrKeys = new int[32];
+    public int[] arrKeys = new int[32];
     private string[] arrValues = new string[32];
 
     //Методы словаря 
     public void Add(int key, string value)
     {
-        arrKeys[counter] = key;
-        arrValues[counter] = value;
-        counter++;
+        try
+        {
+            arrKeys[counter] = key;
+            arrValues[counter] = value;
+            counter++;
+        }
+        catch
+        {
+            Array.Resize(ref arrKeys, arrKeys.Length * 2);
+            Array.Resize(ref arrValues, arrValues.Length * 2);
+        }
+
     }
     public string Get(int key)
     {
-        return "";
-
+        try
+        {
+            return arrValues[key];
+        }
+        catch (Exception)
+        {
+            return "Неверное значение ключа.";
+        }
     }
 }
 
